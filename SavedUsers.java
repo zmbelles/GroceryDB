baa56e6ff4e8942d80e7a40fa9efb39a6e1c0ecc
Zachary Belles <zmbelles@noctrl.edu>
2022-11-21 10:03:35 -0600

    some bug fixes
    some bugs fixed. will edit more later

--- SavedUsers.java
+++ SavedUsers.java
@@ -13,28 +13,13 @@ public class SavedUsers {
 	    lineScanner.useDelimiter(",");
 	    while(lineScanner.hasNext()){
 	    	String uName = lineScanner.next();
-	    	if(uName==userAttempt) {
+	    	if(uName.equals(userAttempt)) {
 	    		return true;
 	    	}
 	    	//dump the part we dont need
 	    	lineScanner.next();
 	    }
-		
-		return false;
-	}
-	
-	private static boolean hashFound(String thisSavedUser, String userAttempt) {
-		Scanner lineScanner = new Scanner(thisSavedUser);
-	    lineScanner.useDelimiter(",");
-	    while(lineScanner.hasNext()){
-	    	String uName = lineScanner.next();
-	    	if(uName==userAttempt) {
-	    		return true;
-	    	}
-	    	//dump the part we dont need
-	    	lineScanner.next();
-	    }
-		
+		lineScanner.close();
 		return false;
 	}
 	public boolean exists(String name, String hash) throws IOException {
@@ -46,17 +31,26 @@ public class SavedUsers {
 		String thisHash;
 		boolean uFound = false;
 		boolean hFound = true;
+		
 		/*the number of users will always equal the number of hashes*/
-		while(br.readLine() != null) {
+		do {
 			//TODO: change it so both files are read here 
 			thisUser = br.readLine();
 			thisHash = br1.readLine();
+			String hashAttempt = Integer.toString(hash.hashCode());
+			
 			boolean tmp1 = userFound(thisUser, name);
-			boolean tmp2 = (hash == thisHash);
+			boolean tmp2 = (thisHash.equals(hashAttempt));
 			if(tmp1==true) {uFound = true;}
 			if(tmp2==true) {hFound = true;}
+		}while(br.readLine() != null);
+		if(uFound && hFound) {
+			br1.close();
+			br.close();
+			return true;
 		}
-		if(uFound && hFound) {return true;}
+		br1.close();
+		br.close();
 		return false;
 	}
 }
